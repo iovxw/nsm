06207b3aa56abbb46d4aa9be12d42d7f3e2523cd
Bluek404 <i@bluek404.net>
2015-04-30 20:02:04 +0800

    use qml

---
+++ assets/main.qml
@@ -0,0 +1,56 @@
+import QtQuick 2.2
+import QtQuick.Window 2.0
+import QtQuick.Controls 1.1
+
+ApplicationWindow {
+    id: mainwindow
+    flags: Qt.FramelessWindowHint | Qt.WindowStaysOnTopHint
+    visible: true
+    title: qsTr("Network Speed Monitor")
+    width: 80
+    height: 37
+    x: (Screen.width - width)
+    y: (Screen.height - height)
+    color: "transparent"
+
+    Rectangle {
+        anchors.fill: parent
+        radius: 2
+        opacity: 0.7
+        color: "black"
+    }
+
+    MouseArea {
+        id: mouseRegion
+        anchors.fill: parent;
+
+        // 用于记录鼠标开始拖动的位置
+        property variant clickPos: "0, 0"
+
+        onPressed: {
+            clickPos  = Qt.point(mouse.x, mouse.y)
+        }
+
+        onPositionChanged: {
+            mainwindow.x = mainwindow.x + mouse.x - clickPos.x
+            mainwindow.y = mainwindow.y + mouse.y - clickPos.y
+        }
+
+        Text {
+            id: up
+            x: 3
+            color: "white"
+            text: speed.up
+            font.pixelSize: 13
+        }
+        Text {
+            id: down
+            x: 3
+            y: up.y + font.pixelSize + 3
+            color: "white"
+            text: speed.down
+            font.pixelSize: 13
+        }
+    }
+}
+
---
+++ main.go
@@ -0,0 +1,134 @@
+package main
+
+//go:generate genqrc assets
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"strconv"
+	"time"
+
+	"gopkg.in/qml.v1"
+)
+
+var net string
+
+func readIntFromFile(path string) (int, error) {
+	b, err := ioutil.ReadFile(path)
+	if err != nil {
+		return 0, err
+	}
+	b = b[:len(b)-1] // delete "\n"
+	i, err := strconv.Atoi(string(b))
+	if err != nil {
+		return 0, err
+	}
+	return i, nil
+}
+
+func getNetIn() int {
+	i, err := readIntFromFile("/sys/class/net/" + net + "/statistics/rx_bytes")
+	if err != nil {
+		log.Fatal(err)
+	}
+	return i
+}
+
+func getNetOut() int {
+	i, err := readIntFromFile("/sys/class/net/" + net + "/statistics/tx_bytes")
+	if err != nil {
+		log.Fatal(err)
+	}
+	return i
+}
+
+func parseSize(src int) string {
+	if src == 0 {
+		return "0 B/s"
+	}
+	var unit string
+
+	for i := 0; ; i++ {
+		size := 1 << uint(i*10)
+		if src < size {
+			size = 1 << uint((i-1)*10)
+			src = src / size
+			switch i - 1 {
+			case 0:
+				unit = "B"
+			case 1:
+				unit = "KB"
+			case 2:
+				unit = "MB"
+			case 3:
+				unit = "GB"
+			}
+			break
+		}
+	}
+	return strconv.Itoa(src) + " " + unit + "/s"
+}
+
+func main() {
+	if len(os.Args) == 1 {
+		net = "wlan0"
+	} else {
+		net = os.Args[1]
+	}
+
+	if err := qml.Run(run); err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(1)
+	}
+}
+
+func run() error {
+	engine := qml.NewEngine()
+	component, err := engine.LoadFile("qrc:///assets/main.qml")
+	if err != nil {
+		return err
+	}
+
+	speed := Speed{
+		Up:   `<font color="green">⇧</font> `,
+		Down: `<font color="red">⇩</font> `,
+	}
+
+	context := engine.Context()
+	context.SetVar("speed", &speed)
+
+	window := component.CreateWindow(nil)
+
+	speed.Root = window.Root()
+
+	window.Show()
+
+	go func() {
+		var (
+			oldNetIn  = getNetIn()
+			oldNetOut = getNetOut()
+			ticker    = time.NewTicker(time.Second * 1)
+		)
+		for _ = range ticker.C {
+			netIn := getNetIn()
+			netOut := getNetOut()
+			speed.Up = `<font color="green">⇧</font> ` + parseSize(netIn-oldNetIn)
+			speed.Down = `<font color="red">⇩</font> ` + parseSize(netOut-oldNetOut)
+			oldNetIn = netIn
+			oldNetOut = netOut
+			context.SetVar("speed", &speed)
+		}
+	}()
+
+	window.Wait()
+
+	return nil
+}
+
+type Speed struct {
+	Root qml.Object
+	Up   string
+	Down string
+}
